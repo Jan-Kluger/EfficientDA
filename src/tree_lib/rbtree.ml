d3ec7ee00e681ee6d590751abb3aa95bd2d5f5c2
yannick <yannick@schuermann.org>
2024-12-27 18:50:57 -0500

    reformatting

--- src/tree_lib/rbtree.ml
+++ src/tree_lib/rbtree.ml
@@ -1,12 +1,15 @@
+open Order_lib.Orders
+
 type color = Red | Black
 
 type 'a rb_node =
   | Node of color * 'a rb_node * 'a rb_node
   | Leaf
 
-module RbTree : Tree_sig.TREE with type 'a t = 'a rb_node = struct
+(* RB Tree implementation *)
+module RbTree (Order : Order_lib.Order_sig.ORDER) : Tree_sig.TREE with type 'a t = 'a rb_node = struct
   type 'a t = 'a rb_node
-  
+
   let insert _ _ = 
     failwith "todo"
   
@@ -22,3 +25,12 @@ module RbTree : Tree_sig.TREE with type 'a t = 'a rb_node = struct
 
   let max _ = failwith "TODO"
 end
+
+module Int_RBTree = RbTree(IntOrder)
+
+let splice (arr : int array) (idx : int) : int array =
+  (Array.sub arr (idx + 1) (Array.length arr - idx - 1)) |> Array.append (Array.sub arr 0 idx)
+
+let testarr = [|0;1;2;3;4;5;6;7;8;9|]
+
+let tres = splice testarr 2
