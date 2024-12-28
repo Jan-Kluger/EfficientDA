2736716de121372173162e43c02877cb1fb534a0
yannick <yannick@schuermann.org>
2024-12-27 19:19:56 -0500

    worked on rbtree

--- src/tree_lib/rbtree.ml
+++ src/tree_lib/rbtree.ml
@@ -1,3 +1,5 @@
+[@@@ warning "-32"]
+
 open Order_lib.Orders
 
 type color = Red | Black
@@ -9,6 +11,21 @@ type 'a rb_node =
 (* RB Tree implementation *)
 module RbTree (Order : Order_lib.Order_sig.ORDER) : Tree_sig.TREE with type 'a t = 'a rb_node = struct
   type 'a t = 'a rb_node
+
+  let [@ inline] rec black_height (node : 'a t) : int =
+    match node with
+    | Node (c, l, _) -> if c = Black then (black_height l + 1) else black_height l
+    | Leaf -> 1
+    
+    let rec black_height_check (node : 'a t) : int =
+      match node with
+      | Node (c, l, r) ->
+          let left_height = black_height l in
+          let right_height = black_height r in
+          if left_height <> right_height then
+            failwith "Inconsistent black height in the tree";
+          if c = Black then left_height + 1 else left_height
+      | Leaf -> 1
 
   let insert _ _ = 
     failwith "todo"
