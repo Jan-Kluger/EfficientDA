3f78f65ded7bf473cb973efca44e3a20a2db52b8
yannick <yannick@schuermann.org>
2025-02-24 14:51:58 +0100

    added string order

--- src/order_lib/orders.ml
+++ src/order_lib/orders.ml
@@ -6,3 +6,14 @@ module IntOrder : ORDER with type t = int = struct
   let compare x y =
     if x = y then Equal else if x > y then Greater else Lesser
 end
+
+module StringOrder : ORDER with type t = string = struct
+  type t = string
+
+  let compare x y : order=
+    match String.compare x y with
+    | 0 -> Equal
+    | x when (x > 0) -> Greater
+    | x when (x < 0) -> Lesser
+    | _ -> failwith "cant"
+end
